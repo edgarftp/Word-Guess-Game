1e623b5d3faec3f2644f7e6152456b49218be256
edgarftp <edgarftp@gmail.com>
2018-05-01 11:37:30 -0500

    some DRY on game mode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 
 var movies = {
 
-j: 5,
+
 under: [],
 userGuess: "",
 oscarsArray : ["the shape of water", "moonlight","spotlight","birdman", "12 years a slave", "argo", "the artist", "the hurt locker", "slumdog millionaire", "no country for old men", "the departed", "crash", "chicago", "a beautiful mind", "gladiator", "american beauty", "shakespeare in love", "titanic", "the english patient", "braveheart", "forest gump", "unforgiven", "the silence of the lamb", "dances with wolves", "driving miss daisy", "rain man", "the last emperor", "platoon", "out of africa", "amadeus", "terms of endearment", "ghandi", "chariots of fire", "ordinary people" ],
@@ -36,19 +36,20 @@ start_Game= function(){
     document.getElementById("gameMode").style.display = "flex";
 } 
 
-easy_mode = function (){
+game_mode = function (j){
     document.getElementById("gameMode").style.display = "none";
     document.getElementById("livesDiv").style.display = "block";
     document.getElementById("tried").style.display = "block";
+    document.getElementById("lives").innerHTML = j;
     movies.pick_word();
     document.onkeyup = function(event) {
 
         console.log(event);
         movies.userGuess = event.key.toLowerCase();
         var wrongGuess = true;
-        document.getElementById("lives").innerHTML = movies.j;
+        document.getElementById("lives").innerHTML = j;
         
-            if (((event.keyCode >= 48 && event.keyCode <=57) ||(event.keyCode >= 65 && event.keyCode <= 90)) && (movies.triedArray.indexOf(event.key)<0) && movies.j > 0) {
+            if (((event.keyCode >= 48 && event.keyCode <=57) ||(event.keyCode >= 65 && event.keyCode <= 90)) && (movies.triedArray.indexOf(event.key)<0) && j > 0) {
                 for (i=0 ; i < movies.randPick.length ; i++){
                     if (movies.userGuess == movies.randPick.charAt(i)){
                         console.log(movies.userGuess);
@@ -60,86 +61,15 @@ easy_mode = function (){
                 }
 
                 if (wrongGuess) {
-                    movies.j-=1;
+                    j-=1;
                     wrongGuess = true;
                     movies.triedArray.push(movies.userGuess);
                     movies.triedArray.push("&nbsp;");
                     movies.joinTried= movies.triedArray.join("");
                     document.getElementById("tried").innerHTML = ("You have already tried: " + movies.joinTried);
-                    document.getElementById("lives").innerHTML = movies.j;
+                    document.getElementById("lives").innerHTML = j;
                 }
             }
     }
 }
 
-medium_mode = function (){
-    document.getElementById("gameMode").style.display = "none";
-    document.getElementById("livesDiv").style.display = "block";
-    document.getElementById("tried").style.display = "block";
-    movies.pick_word();
-    document.onkeyup = function(event) {
-
-        console.log(event);
-        movies.userGuess = event.key.toLowerCase();
-        var wrongGuess = true;
-        document.getElementById("lives").innerHTML = movies.j;
-        
-            if (((event.keyCode >= 48 && event.keyCode <=57) ||(event.keyCode >= 65 && event.keyCode <= 90)) && (movies.triedArray.indexOf(event.key)<0) && movies.j > 0) {
-                for (i=0 ; i < movies.randPick.length ; i++){
-                    if (movies.userGuess == movies.randPick.charAt(i)){
-                        console.log(movies.userGuess);
-                        movies.under[i] = (movies.userGuess + "&nbsp;" + "&nbsp;");
-                        movies.joinUnder = movies.under.join("");
-                        document.getElementById("underscores").innerHTML = movies.joinUnder;
-                        wrongGuess = false;
-                    }
-                }
-
-                if (wrongGuess) {
-                    movies.j-=1;
-                    wrongGuess = true;
-                    movies.triedArray.push(movies.userGuess);
-                    movies.triedArray.push("&nbsp;");
-                    movies.joinTried= movies.triedArray.join("");
-                    document.getElementById("tried").innerHTML = ("You have already tried: " + movies.joinTried);
-                    document.getElementById("lives").innerHTML = movies.j;
-                }
-            }
-    }
-}
-
-hard_mode = function (){
-    document.getElementById("gameMode").style.display = "none";
-    document.getElementById("livesDiv").style.display = "block";
-    document.getElementById("tried").style.display = "block";
-    movies.pick_word();
-    document.onkeyup = function(event) {
-
-        console.log(event);
-        movies.userGuess = event.key.toLowerCase();
-        var wrongGuess = true;
-        document.getElementById("lives").innerHTML = movies.j;
-        
-            if (((event.keyCode >= 48 && event.keyCode <=57) ||(event.keyCode >= 65 && event.keyCode <= 90)) && (movies.triedArray.indexOf(event.key)<0) && movies.j > 0) {
-                for (i=0 ; i < movies.randPick.length ; i++){
-                    if (movies.userGuess == movies.randPick.charAt(i)){
-                        console.log(movies.userGuess);
-                        movies.under[i] = (movies.userGuess + "&nbsp;" + "&nbsp;");
-                        movies.joinUnder = movies.under.join("");
-                        document.getElementById("underscores").innerHTML = movies.joinUnder;
-                        wrongGuess = false;
-                    }
-                }
-
-                if (wrongGuess) {
-                    movies.j-=1;
-                    wrongGuess = true;
-                    movies.triedArray.push(movies.userGuess);
-                    movies.triedArray.push("&nbsp;");
-                    movies.joinTried= movies.triedArray.join("");
-                    document.getElementById("tried").innerHTML = ("You have already tried: " + movies.joinTried);
-                    document.getElementById("lives").innerHTML = movies.j;
-                }
-            }
-    }
-}
